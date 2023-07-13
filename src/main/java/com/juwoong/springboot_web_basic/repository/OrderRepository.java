16a65916e4ddc18db3018be624238f07e2eb7c35
Juwoong <wwong7247@gmail.com>
2023-07-13 17:07:17 +0900

    Order 레포지토리 구현

---
+++ src/main/java/com/juwoong/springboot_web_basic/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.juwoong.springboot_web_basic.repository;
+
+import com.juwoong.springboot_web_basic.model.Order;
+
+public interface OrderRepository {
+
+    Order insert(Order order);
+
+}
