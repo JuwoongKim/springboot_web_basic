e9dbc5c23a81aa9685bde912244f5b998aff18e6
Juwoong <wwong7247@gmail.com>
2023-07-13 15:58:58 +0900

    초기 프로젝트 구성

---
+++ src/main/java/com/juwoong/springboot_web_basic/SpringbootWebBasicApplication.java
@@ -0,0 +1,13 @@
+package com.juwoong.springboot_web_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juwoong/springboot_web_basic/SpringbootWebBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwoong.springboot_web_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
